d61d68002fe19bbf33d71cb76570a710f78c7cad
Sianybo <sian_ayres01@yahoo.co.uk>
2023-04-26 14:24:56 +0100

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -6,10 +6,10 @@ class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
-    def load(self, metadata, cls, string):
+    def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
-        load(fm, Loader=FullLoader)
-        cls(metadata, content)
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
 
     def __init__(self, metadata, content):
         self.data = metadata
