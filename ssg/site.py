7b98355446e2f4caf2d734ef5b1c8ce3be40db7c
Sianybo <sian_ayres01@yahoo.co.uk>
2023-04-26 14:01:53 +0100

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -8,9 +8,10 @@ class Site:
 
     def create_dir(self, path):
         directory = self.dest/path.relative_to(self.source)
-
+        directory.mkdir(parents=True, exist_ok=True)
+        
     def build(self):
-        self.directory.mkdir(parents=True, exist_ok=True)
+        self.dest.mkdir(parents=True, exist_ok=True)
 
         for path in self.source.rglob("*"):
             if path.is_dir():
