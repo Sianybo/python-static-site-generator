c3e2ecc8428f745c72d9894b822d0cdc7b327787
Sianybo <sian_ayres01@yahoo.co.uk>
2023-04-26 14:16:37 +0100

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -4,12 +4,38 @@ from yaml import FullLoader
 from collections.abc import Mapping
 
 class Content:
-    __delimeter = "^(?:-|\+){3}\s*$"
+    __delimeter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimeter, re.MULTILINE)
 
     def load(self, cls, string):
-        _, fm, content = regex.split(string, 2)
+        _, fm, content = cls.__regex.split(string, 2)
         load(fm, Loader=FullLoader)
         cls(metadata, content)
 
-    
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data["content"] = content
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        if "type" in self.data:
+            return self.data["type"]
+        else:
+            return None
+
+    @type.setter
+    def type(self, type):
+        return self.data["type"]
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
