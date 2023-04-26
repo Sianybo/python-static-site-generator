e517479917552445a564fbe24526bdac5f073a42
Sianybo <sian_ayres01@yahoo.co.uk>
2023-04-26 13:57:33 +0100

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 class Site:
-    def __init__(self, source, dest, parsers= "None"):
+    def __init__(self, source, dest, parsers = None):
         self.source = Path(source)
         self.dest = Path(dest)
         self.parsers = parsers or []
@@ -16,16 +16,16 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
-                run_parser(path)
+                self.run_parser(path)
 
     def load_parser(self, extension):
         for parser in self.parsers:
-            if extension.valid_extension():
+            if extension.valid_extension(extension):
                 return parser
 
     def run_parser(self, path):
-        parser = load_parser(path.suffix)
+        parser = self.load_parser(path.suffix)
         if parser is not None:
-            parse(path, source, dest)
+            parser.parse(path, source, dest)
         else:
             print("Not Implemented")
